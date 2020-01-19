c906d8401f8c5b778f3d2d55df807f1417113c76
lacazeto <lucas.cazeto@gmail.com>
2020-01-19 20:44:04 +0100

    WIP: adding logic to article C

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,20 +2,46 @@
 
 class ArticlesController < ApplicationController
   # GET
-  def articles; end
+  def articles
+    @articles = Article.all
+  end
 
-  def new; end
+  def new
+    @article = Article.new
+  end
 
-  def show; end
+  def show
+    @article = Article.find(params[:id].to_i)
+  end
 
-  def edit; end
+  def edit
+    @article = Article.find(params[:id].to_i)
+  end
 
   # POST
-  def create; end
+  def create
+    @article = Article.new(article_params)
+
+    respond_to do |format|
+      if @article.save
+        format.html { redirect_to @article, notice: 'Article was successfully created.' }
+        render json: @article, status: :created
+      else
+        format.html { render :new }
+        render json: { errors: @article.errors }, status: :unprocessable_entity
+      end
+    end
+  end
 
   # PUT
   def update; end
 
   # DELETE
   def destroy; end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content, :author)
+  end
 end
