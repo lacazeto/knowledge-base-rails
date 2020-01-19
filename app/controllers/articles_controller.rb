e2896eab0fa7494b2e7ede4e1b9cf980feb5353c
lacazeto <lucas.cazeto@gmail.com>
2020-01-19 17:29:52 +0100

    WIP: added article routes + model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,21 @@
 # frozen_string_literal: true
 
 class ArticlesController < ApplicationController
+  # GET
   def articles; end
+
+  def new; end
+
+  def show; end
+
+  def edit; end
+
+  # POST
+  def create; end
+
+  # PUT
+  def update; end
+
+  # DELETE
+  def destroy; end
 end
