602e57013ce6f3efab96f7ae418e5adb5a892c2f
lacazeto <lucas.cazeto@gmail.com>
2020-01-20 01:06:20 +0100

    db: added validation to tables

--- db/migrate/20200119162544_create_articles.rb
+++ db/migrate/20200119162544_create_articles.rb
@@ -3,9 +3,9 @@
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
-      t.string :title
-      t.text :content
-      t.string :author
+      t.string :title, null: false
+      t.text :content, null: false
+      t.string :author, null: false
 
       t.timestamps
     end
--- db/migrate/20200119164218_create_questions.rb
+++ db/migrate/20200119164218_create_questions.rb
@@ -3,9 +3,9 @@
 class CreateQuestions < ActiveRecord::Migration[6.0]
   def change
     create_table :questions do |t|
-      t.string :title
-      t.text :content
-      t.string :author
+      t.string :title, null: false
+      t.text :content, null: false
+      t.string :author, null: false
       t.references :article, null: false, foreign_key: true
 
       t.timestamps
