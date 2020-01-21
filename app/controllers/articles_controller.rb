31e871335efe2fcb4b8bf4ccf7bb611b17bb633f
lacazeto <lucas.cazeto@gmail.com>
2020-01-21 23:13:51 +0100

    minor

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -47,6 +47,6 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    params.require(:article).permit(:title, :content, :author)
+    params.require(:article).permit(:title, :content_original, :author)
   end
 end
